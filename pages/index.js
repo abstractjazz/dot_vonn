707c52ae2ce9d7e11f74d9ccd1d697c75f552d18
abstractjazz <evan@heyevan.com>
2022-08-22 14:18:15 -0700

    initial commit w/Tailwind

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,7 +14,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className="text-9xl">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
